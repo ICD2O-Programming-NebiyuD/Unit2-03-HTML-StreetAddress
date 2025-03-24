e77c55b4906969c00adc4d1a67611ad0b52e4368
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-03-24 14:53:27 +0000

    I made a user inputer for street number and name

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ms Raffin
+// Created on: Sep 2022
 // This file contains the JS functions for index.html
+
+function enterClicked () {
+  // this function gets user's first name and age and displays it back to user
+
+  // get first name from text field
+  let streetName = document.getElementById("street-name").value
+	// get age from text field and cast it to integer
+  let streetNumber = parseInt(document.getElementById("number-entered").value)
+
+  // display name and age back to user
+  document.getElementById('user-info').innerHTML = "Your Street name is " + streetName + " and your Street Number is " + streetNumber + "."
+}
